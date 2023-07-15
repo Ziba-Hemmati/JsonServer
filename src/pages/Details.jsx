a4c95b2252645d99912d2957450cc1058ec472ea
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-15 16:38:54 +0330

    feat: add details styles

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "../styles/details.scss";
 
 const CONTACTS_API = "http://localhost:3000/contacts";
 
 const Details = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const [contact, setContact] = useState({ name: "", number: "" });
   const { name, number } = contact;
@@ -20,8 +22,19 @@ const Details = () => {
     };
     getContacts();
   }, []);
-
-  return <div></div>;
+  const handleBack = () => navigate(-1);
+  return (
+    <div className="details-container">
+      <div className="details-card">
+        <i className="fa-solid fa-user"></i>
+        <p> Contact information : </p>
+        <p>Contact Name : {name}</p>
+        <p>Contact Number : {number}</p>
+        <p>ID : {params.id}</p>
+        <button onClick={handleBack}>Back</button>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
